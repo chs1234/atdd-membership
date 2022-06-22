69e8f56f2228371f3b27a780535f0e9d106ec60e
채희승 <chs4413@gmail.com>
2022-06-23 00:22:38 +0900

    Membership 테스트코드 수정

--- src/test/java/com/seung/atdd/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/seung/atdd/membership/repository/MembershipRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.seung.atdd.membership.repository;
 
+import com.seung.atdd.membership.entity.Membership;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,7 +14,21 @@ public class MembershipRepositoryTest {
     private MembershipRepository membershipRepository;
 
     @Test
-    public void MembershipRepository가Null이아님() {
-        assertThat(membershipRepository).isNotNull();
+    public void 멤버십등록() {
+        // given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipName("네이버")
+                .point(10000)
+                .build();
+
+        // when
+        final Membership result = membershipRepository.save(membership);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getUserId()).isEqualTo("userId");
+        assertThat(result.getMembershipName()).isEqualTo("네이버");
+        assertThat(result.getPoint()).isEqualTo(10000);
     }
 }
