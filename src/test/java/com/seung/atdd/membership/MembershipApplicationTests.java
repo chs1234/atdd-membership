1ac2688e09a924d59219b3299fc4e590adb72b5e
채희승 <chs4413@gmail.com>
2022-06-22 23:20:24 +0900

    tdd example initial commit

---
+++ src/test/java/com/seung/atdd/membership/MembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seung.atdd.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
