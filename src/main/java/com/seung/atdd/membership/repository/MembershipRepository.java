7050998c34955dce471e5f5bbf69b3551b71a70a
채희승 <chs4413@gmail.com>
2022-06-23 00:39:52 +0900

    테스트코드 추가: 멤버십이 존재하는지

--- src/main/java/com/seung/atdd/membership/repository/MembershipRepository.java
+++ src/main/java/com/seung/atdd/membership/repository/MembershipRepository.java
@@ -1,7 +1,10 @@
 package com.seung.atdd.membership.repository;
 
 import com.seung.atdd.membership.entity.Membership;
+import com.seung.atdd.membership.enums.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+
+    Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
 }
